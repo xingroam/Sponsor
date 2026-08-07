287778df0c895b9d8fda4cbf5e2757f905d6748c
X <x@potor.com>
2026-08-08 03:14:29 +0800

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://xingroam.github.io/',
-  base: '/sponsor',
+  site: 'https://xingroam.github.io/sponsor',
+  base: '/',
 });
